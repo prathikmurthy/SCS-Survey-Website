a77dd98e88d1656b8e9593e70a1b97eb8fac001f
Prathik M <murthy.prathik@gmail.com>
2022-05-31 14:39:50 -0400

    .

--- components/Tile.js
+++ components/Tile.js
@@ -5,14 +5,6 @@ import Image from 'next/image'
 export default function Tile(props) {
 
     const [color, setColor] = useState('[#191919]')
-    // var title;
-
-    // if (props.title == undefined) {
-    //     title = 'Tile'
-    // } else {
-    //     title = props.title
-    // }
-
 
 
     const update = () => {
@@ -43,20 +35,16 @@ export default function Tile(props) {
     }
 
     return (
-        <div className = {`pt-5 pb-10 rounded-2xl bg-${color} hover:scale-110 pl-2 pr-2 transition ease-in-out` } onClick={update} >
+        <div className = {`w-max-md pt-5 rounded-2xl bg-${color} hover:scale-110 pl-2 pr-2 transition ease-in-out` } >
             <div className="relative">
-                {/* <Image 
-                    src="https://picsum.photos/400/300"
-                    alt="picture"
-                    className="w-full rounded-xl"
-                /> */}
-                <img src="https://picsum.photos/400/300" className="w-full rounded-xl"></img>
+                <img src={props.pi.image} className="w-full rounded-xl"></img>
+                {/* <img src="https://picsum.photos/400/300" className="w-full rounded-xl"></img> */}
                 {/* <img src="https://images.steelcase.com/image/upload/c_fill,q_auto,f_auto,h_900,w_1600/v1623221806/www.steelcase.com/2021/06/09/21-0160647.jpg" className="w-full rounded-xl"></img> */}
-                <button className="absolute inset-0 pl-4 pr-4 pt-2 pb-2 text-white font-bold rounded opacity-0 hover:opacity-100 bg-black/50 transition ease-in-out">Details</button> 
+                
+                <button className="absolute inset-0 pl-4 pr-4 pt-2 pb-2 text-white text-center self-center align-middle font-bold rounded opacity-0 hover:opacity-100 bg-black/50 transition ease-in-out"> <a href={props.pi.url} target="_blank">Details</a> </button> 
             </div>
-            <p className="text-white font-bold text-center pt-5 ">{props.pi.id}</p>
-            {/* <Selection />                 */}
-            <p className="text-slate-500 italic text-center pt-5">NOTE: {props.pi.notes}</p>
+            <p className="text-white font-bold text-center pt-5 "  onClick={update}>{props.pi.id}</p>
+            <p className="text-slate-500 italic text-center pt-5 pb-10" onClick={update}>{props.pi.notes != "" ? "Notes: " + props.pi.notes : props.pi.notes}</p>
             
         </div>
     )
--- pages/api/db.js
+++ pages/api/db.js
@@ -4,35 +4,61 @@ require('dotenv').config();
 const id = process.env.MONGO_API;
 const db_name = "Planning-Idea-Survey";
 const collection = "00" 
+const input_collection = "01"
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 export default async function handler(req, res) {
-    let m = new Mongo(new MongoClient(id,  { 
-                                                useNewUrlParser: true,
-                                                useUnifiedTopology: true, 
-                                                serverApi: ServerApiVersion.v1 
-                                            }), db_name, collection);
-
-    await m.init();
-
+    
     switch (req.method) {
         case 'POST': 
+            var m = new Mongo(new MongoClient(id,  { 
+                                                        useNewUrlParser: true,
+                                                        useUnifiedTopology: true, 
+                                                        serverApi: ServerApiVersion.v1 
+                                                    }), db_name, collection);
+        
+            await m.init();
+            
             console.log(req.body);
-        
+            
             const d = new Date();
-        
+            
             req.body['date'] = d.toString();
-        
+            
             await m.add(req.body);
             
             res.status(200).json( {res: 'Success'} )
-
+            
             break;
-        
+            
         case 'DELETE':
+            var m = new Mongo(new MongoClient(id,  { 
+                                                        useNewUrlParser: true,
+                                                        useUnifiedTopology: true, 
+                                                        serverApi: ServerApiVersion.v1 
+                                                    }), db_name, collection);
+        
+            await m.init();
             await m.cleardb();
             res.status(200).json( {res: 'Success'})
+            
+            break;
+                
+        case 'GET':
+                
+            var m = new Mongo(new MongoClient(id,  { 
+                                                        useNewUrlParser: true,
+                                                        useUnifiedTopology: true, 
+                                                        serverApi: ServerApiVersion.v1 
+                                                    }), db_name, input_collection);
+        
+            await m.init();
+
+            res.status(200).json( {res: await m.find({ _id: 'INPUTDATA' }) })
+            break;
+
         }
+
 
     await m.close();
   }
