559719c2916419f97ba8abda666d4220b4a9bdc0
Prathik M <murthy.prathik@gmail.com>
2022-06-08 11:06:32 -0400

    .

--- components/Tile.js
+++ components/Tile.js
@@ -4,15 +4,15 @@ import Image from 'next/image'
 
 export default function Tile(props) {
 
-    const [color, setColor] = useState(false)
+    const [color, setColor] = useState('[#191919]')
 
 
     const update = () => {
-        if (color) {
-            setColor(true)
+        if (color == '[#191919]') {
+            setColor('green-500')
             add()
         } else {
-            setColor(false)
+            setColor('[#191919]')
             sub()
         }
     }
@@ -35,7 +35,7 @@ export default function Tile(props) {
     }
 
     return (
-        <div className = {`w-max-md pt-5 rounded-2xl bg-${color ? 'green-500' : '[#191919]'} hover:scale-110 pl-2 pr-2 transition ease-in-out` } >
+        <div className = {`w-max-md pt-5 rounded-2xl bg-${color} hover:scale-110 pl-2 pr-2 transition ease-in-out` } >
             <div className="relative">
                 <img src={props.pi.image} className="w-full rounded-xl"></img>                
                 <button className="absolute inset-0 pl-4 pr-4 pt-2 pb-2 text-white text-center self-center align-middle font-bold rounded opacity-0 hover:opacity-100 bg-black/50 transition ease-in-out"> <a href={props.pi.url} target="_blank" rel="noreferrer">Details</a> </button> 
